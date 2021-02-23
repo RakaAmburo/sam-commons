e6bc7b967017de2a350b7400cc07cedf8d083399
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2021-02-23 14:49:34 +0100

    adding

--- src/main/java/com/sam/commons/entities/GroupsDTO.java
+++ src/main/java/com/sam/commons/entities/GroupsDTO.java
@@ -1,8 +1,14 @@
 package com.sam.commons.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupsDTO {
 
     private String id;
